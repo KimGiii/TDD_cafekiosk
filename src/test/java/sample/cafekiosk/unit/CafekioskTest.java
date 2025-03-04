9573ee426c298f62b3f0b556fe2536d9d8f14f49
KimGiii <gibo9809@gmail.com>
2025-03-04 16:29:43 +0900

    테스트하기 어려운 영역(주문 시간) 분리

--- src/test/java/sample/cafekiosk/unit/CafekioskTest.java
+++ src/test/java/sample/cafekiosk/unit/CafekioskTest.java
@@ -9,7 +9,6 @@ import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 class CafekioskTest {
 
